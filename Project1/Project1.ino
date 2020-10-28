1dc20632873a7ebe162debb1fa356281b0615420
mikeyangelo5 <71859576+mikeyangelo5@users.noreply.github.com>
2020-10-28 12:51:02 +0700

    Add files via upload

---
+++ Project1/Project1.ino
@@ -0,0 +1,47 @@
+//Get GPIO Number to Const
+const int pinLEDR = 23;
+const int pinLEDY = 27;
+const int pinLEDG = 26;
+
+//Set all LED state to LOW
+int RState = LOW;
+int YState = LOW;
+int GState = LOW;
+
+//Variables for Save Time and Interval Time in LED
+long SavedTime = 0;
+long IntervalTime = 10000;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pinLEDR, OUTPUT);
+  pinMode(pinLEDY, OUTPUT);
+  pinMode(pinLEDG, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned long TimeNow = millis();
+  unsigned long ElapsedTime = TimeNow - SavedTime;
+  if(ElapsedTime > IntervalTime){
+    SavedTime = TimeNow;
+  }
+  if(ElapsedTime < 4000){
+    RState = HIGH;
+    YState = LOW;
+    GState = LOW;
+  }
+  if(ElapsedTime > 4000 && ElapsedTime < 8000){
+    RState = LOW;
+    YState = LOW;
+    GState = HIGH;
+  }
+  if(ElapsedTime > 8000){
+    RState = LOW;
+    YState = HIGH;
+    GState = LOW;
+  }
+  digitalWrite(pinLEDR, RState);
+  digitalWrite(pinLEDY, YState);
+  digitalWrite(pinLEDG, GState);
+}
